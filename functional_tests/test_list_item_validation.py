ec7b443f126ebe5bc69e1224ed1e7d5f9e8b82cb
yeahframeoff <Anton.Yefremov@planpro.info>
2016-03-11 15:54:00 +0100

    item input validation FT

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -10,9 +10,13 @@ class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         # User foes to the home page and tries to submit
         # an empty item. Hits enter on the empty input box
+        self.browser.get(self.server_url)
+        self.browser.find_element_by_id('id_new_item').send_keys(Keys.ENTER)
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
+        error = self.browser.find_element_by_css_selector('.has-error')
+        
 
         # The user tries again with some text for the item, and that works
 
