88eb98b1003270453144428a37e82ca094271bb2
yeahframeoff <Anton.Yefremov@planpro.info>
2016-03-16 10:43:39 +0100

    use form in all views, back to working state

--- lists/views.py
+++ lists/views.py
@@ -10,27 +10,21 @@ def home_page(request):
 
 def view_list(request, list_id):
     the_list = List.objects.get(id=list_id)
-    error = None
+    form = ItemForm()
 
     if request.method == 'POST':
-        try:
-            item = Item(text=request.POST['text'], list=the_list)
-            item.full_clean()
-            item.save()
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            Item.objects.create(text=request.POST['text'], list=the_list)
             return redirect(the_list)
-        except ValidationError:
-            error = "You can't have an empty list item"
-    return render(request, 'list.html', {'list': the_list, 'error': error})
+    return render(request, 'list.html', {'list': the_list, 'form': form})
 
 
 def new_list(request):
-    the_list = List.objects.create()
-    item = Item(text=request.POST['text'], list=the_list)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        the_list.delete()
-        error = "You can't have an empty list item"
-        return render(request, 'home.html', {'error': error})
-    return redirect(the_list)
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        the_list = List.objects.create()        
+        item = Item.objects.create(text=request.POST['text'], list=the_list)
+        return redirect(the_list)
+    else:
+        return render(request, 'home.html', {'form': form})
