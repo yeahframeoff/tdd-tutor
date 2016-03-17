88358192ca6cef22bf29348cb7a5fff1e6ed1acc
yeahframeoff <Anton.Yefremov@planpro.info>
2016-03-17 15:00:49 +0100

    initial js tests setup. testing error hiding

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,6 +7,9 @@ from selenium.webdriver.common.keys import Keys
 
 class ItemValidationTest(FunctionalTest):
 
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         # User foes to the home page and tries to submit
         # an empty item. Hits enter on the empty input box
@@ -26,7 +29,7 @@ class ItemValidationTest(FunctionalTest):
         self.get_element_input_box().send_keys(Keys.ENTER)
         # She receives a similar warning on the list page
         self.check_for_rows_in_list_table('#1: Buy milk')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         # And he fixes it by inputting some text in
@@ -45,5 +48,19 @@ class ItemValidationTest(FunctionalTest):
 
         # Sees a helpful error message 
         self.check_for_rows_in_list_table('#1: Cook waffles')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
+
+    def test_error_messaages_are_cleared_on_input(self):
+        # User starts a new list in a way that causes a validation error
+        self.browser.get(self.server_url)
+        self.get_element_input_box().send_keys(Keys.ENTER)
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        # user starts typing in the input box to clear the error
+        self.get_element_input_box().send_keys('a')
+
+        # users is pleased to see that error message dissapears
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
