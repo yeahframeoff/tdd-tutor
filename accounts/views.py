4b38bb0602f78f00130a66057181b449b73f47cf
yeahframeoff <anthyef@gmail.com>
2016-04-19 22:57:12 +0200

    login tests, accounts urls

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
+def persona_login(request):
+    user = authenticate(assertion=request.POST['assertion'])
+    if user:
+        login(request, user)
+        return HttpResponse('OK')
+    return HttpResponse()
