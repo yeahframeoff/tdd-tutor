9d411fc35c4cfdb0a64860868362c70e43d79fd2
yeahframeoff <Anton.Yefremov@planpro.info>
2016-04-20 15:00:16 +0200

    authentication backend, substituting django original one

---
+++ accounts/authentication.py
@@ -0,0 +1,28 @@
+from django.contrib.auth import get_user_model
+import requests
+User = get_user_model()
+
+
+PERSONA_VERIFY_URL = "https://verifier.login.persona.org/verify"
+DOMAIN = 'localhost'
+
+
+class PersonaAuthenticationBackend:
+
+    def authenticate(self, assertion):
+        response = requests.post(
+            PERSONA_VERIFY_URL,
+            data={'assertion': assertion, 'audience': DOMAIN}
+        )
+        if response.ok and response.json()['status'] == 'okay':
+            email = response.json()['email']
+            try:
+                return User.objects.get(email=email)
+            except User.DoesNotExist:
+                return User.objects.create(email=email)
+
+    def get_user(self, email):
+        try:
+            return User.objects.get(email=email)
+        except User.DoesNotExist:
+            return None
--- superlists/settings.py
+++ superlists/settings.py
@@ -41,6 +41,11 @@ INSTALLED_APPS = (
     'accounts',
 )
 
+AUTH_USER_MODEL = 'accounts.User'
+AUTHENTICATION_BACKENDS = (
+    'accounts.authentication.PersonaAuthenticationBackend',
+)
+
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
